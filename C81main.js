1fb1874526cbe10ab981dbfa750edff3b58f2560
anjika17 <80506928+anjika17@users.noreply.github.com>
2021-09-17 14:14:43 +0530

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -4,21 +4,21 @@ color="pink";
 ctx.beginPath();
 ctx.strokeStyle=color;
 ctx.lineWidth=2;
-ctx.arc(200 , 200 , 40 , 0 , 2*Math.PI);
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
 ctx.stroke();
 canvas.addEventListener("mousedown",draw_circle);
 function draw_circle(e){
 color=document.getElementById("color").value;
 console.log(color);
-   mouse_x=e.clientX - canvas.offsetLeft ;
-   mouse_y=e.clientY - canvas.offsetTop;
+  mouse_x = e.clientX - canvas.offsetLeft ;
+ mouse_y = e.clientY - canvas.offsetTop;
    console.log("X = " + mouse_x + "Y = " + mouse_y);
-   circle(mouse_x , mouse_y)
+   circle(mouse_x , mouse_y);
 }
 function circle(mouse_x , mouse_y){
 ctx.beginPath();
 ctx.strokeStyle=color;
 ctx.lineWidth=2;
-ctx.arc(mouse_x , mouse_y , 40 , 0 , 2*Math.PI);
+ctx.arc(mouse_x, mouse_y, 40, 0, 2*Math.PI);
 ctx.stroke();
 }
